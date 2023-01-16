d013178763635f5d5a3462a66a91cda15c7c01bd
uni51 <unieye51@gmail.com>
2023-01-17 08:06:01 +0900

    Admin認証

---
+++ src/components/Layouts/Admin/AdminAppLayout.tsx
@@ -0,0 +1,24 @@
+import AdminNavigation from './AdminNavigation'
+import { useAdminAuth } from 'hooks/adminAuth'
+
+const AdminAppLayout = ({ header, children }) => {
+  const { admin } = useAdminAuth({ middleware: 'adminAuth' })
+
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <AdminNavigation user={admin} />
+
+      {/* Page Heading */}
+      <header className="bg-white shadow">
+        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          {header}
+        </div>
+      </header>
+
+      {/* Page Content */}
+      <main>{children}</main>
+    </div>
+  )
+}
+
+export default AdminAppLayout
