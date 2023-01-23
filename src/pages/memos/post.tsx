54219e379dbc14c8c4690d5caff7ecd6d709a6db
uni51 <unieye51@gmail.com>
2023-01-24 08:52:44 +0900

    メモ登録機能の実装

---
+++ src/pages/memos/post.tsx
@@ -0,0 +1,108 @@
+import AppLayout from '../../components/Layouts/AppLayout'
+import Head from 'next/head'
+import { AxiosError, AxiosResponse } from 'axios'
+import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import { ChangeEvent, useState } from 'react'
+import { RequiredMark } from '../../components/RequiredMark'
+import axios from '../../lib/axios'
+
+// POSTデータの型
+type MemoForm = {
+  title: string
+  body: string
+}
+
+const Post: NextPage = () => {
+  // ルーター定義
+  const router = useRouter()
+  // state定義
+  const [memoForm, setMemoForm] = useState<MemoForm>({
+    title: '',
+    body: '',
+  })
+  const [validation, setValidation] = useState<MemoForm>({
+    title: '',
+    body: '',
+  })
+
+  // POSTデータの更新
+  const updateMemoForm = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    setMemoForm({ ...memoForm, [e.target.name]: e.target.value })
+  }
+
+  // メモの登録
+  const createMemo = () => {
+    // axios
+    //   // CSRF保護の初期化
+    //   .get('/sanctum/csrf-cookie')
+    //   .then(res => {
+    // APIへのリクエスト
+    axios
+      .post('/api/memos', memoForm)
+      .then((response: AxiosResponse) => {
+        console.log(response.data)
+        router.push('/memos')
+      })
+      .catch((err: AxiosError) => {
+        console.log(err.response)
+      })
+    // })
+  }
+
+  return (
+    <AppLayout
+      header={
+        <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+          Dashboard - メモ一覧
+        </h2>
+      }>
+      <Head>
+        <title>Dashboard - メモ一覧</title>
+      </Head>
+
+      <div className="w-2/3 mx-auto">
+        <div className="w-1/2 mx-auto mt-32 border-2 px-12 py-16 rounded-2xl">
+          <h3 className="mb-10 text-2xl text-center">メモの登録</h3>
+          <div className="mb-5">
+            <div className="flex justify-start my-2">
+              <p>タイトル</p>
+              <RequiredMark />
+            </div>
+            <input
+              className="p-2 border rounded-md w-full outline-none"
+              name="title"
+              value={memoForm.title}
+              onChange={updateMemoForm}
+            />
+          </div>
+          <div className="mb-5">
+            <div className="flex justify-start my-2">
+              <p>メモの内容</p>
+              <RequiredMark />
+            </div>
+            <textarea
+              className="p-2 border rounded-md w-full outline-none"
+              name="body"
+              cols={30}
+              rows={4}
+              value={memoForm.body}
+              onChange={updateMemoForm}
+            />
+          </div>
+          <div className="text-center">
+            <button
+              className="bg-gray-700 text-gray-50 py-3 sm:px-20 px-10 mt-8 rounded-xl cursor-pointer drop-shadow-md hover:bg-gray-600"
+              onClick={createMemo}>
+              登録する
+            </button>
+          </div>
+        </div>
+      </div>
+    </AppLayout>
+  )
+}
+
+export default Post
