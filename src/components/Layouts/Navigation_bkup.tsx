587374e318b425b23fbd288ee71e5f39345978ea
uni51 <unieye51@gmail.com>
2023-01-30 08:29:15 +0900

    AppLayoutの変更

--- src/components/Layouts/Navigation_bkup.tsx
+++ src/components/Layouts/Navigation_bkup.tsx
@@ -20,6 +20,24 @@ const Navigation = ({ user }) => {
       {/* Primary Navigation Menu */}
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
+          <div className="flex">
+            {/* Logo */}
+            <div className="flex-shrink-0 flex items-center">
+              <Link href="/dashboard">
+                <ApplicationLogo className="block h-10 w-auto fill-current text-gray-600" />
+              </Link>
+            </div>
+
+            {/* Navigation Links */}
+            <div className="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+              <NavLink
+                href="/dashboard"
+                active={router.pathname === '/dashboard'}>
+                Dashboard
+              </NavLink>
+            </div>
+          </div>
+
           {/* Settings Dropdown */}
           <div className="hidden sm:flex sm:items-center sm:ml-6">
             <Dropdown
@@ -84,6 +102,14 @@ const Navigation = ({ user }) => {
       {/* Responsive Navigation Menu */}
       {open && (
         <div className="block sm:hidden">
+          <div className="pt-2 pb-3 space-y-1">
+            <ResponsiveNavLink
+              href="/dashboard"
+              active={router.pathname === '/dashboard'}>
+              Dashboard
+            </ResponsiveNavLink>
+          </div>
+
           {/* Responsive Settings Options */}
           <div className="pt-4 pb-1 border-t border-gray-200">
             <div className="flex items-center px-4">
