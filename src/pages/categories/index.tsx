73cb23df2116d3c2626f56a090c7c744600fd0a1
uni51 <unieye51@gmail.com>
2023-05-14 10:15:42 +0900

    カテゴリー一覧 API検証用 02

--- src/pages/categories/index.tsx
+++ src/pages/categories/index.tsx
@@ -5,9 +5,7 @@ import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import { Loading } from '../../components/Loading'
-import { useAuth } from '../../hooks/useAuth'
 import { apiClient } from '../../lib/utils/apiClient'
-import Link from 'next/link'
 
 type Category = {
   name: string
@@ -37,31 +35,21 @@ const CatgeoryList: NextPage = () => {
   if (isLoading) return <Loading />
 
   return (
-    <AppLayout
-      header={
-        <h2 className="font-semibold text-xl text-gray-800 leading-tight">
-          カテゴリー一覧
-        </h2>
-      }>
-      <Head>
-        <title>カテゴリー一覧</title>
-      </Head>
-      <div className="mx-auto mt-32">
-        <div className="w-1/2 mx-auto text-center"></div>
-        <div className="mt-3">
-          {/* DBから取得したメモデータの一覧表示 */}
-          <div className="grid w-4/5 mx-auto gap-4 grid-cols-2">
-            {categories.map((category: Category, index) => {
-              return (
-                <div className="bg-gray-100 shadow-lg mb-5 p-4">
-                  <p className="text-lg font-bold mb-1">{category.name}</p>
-                </div>
-              )
-            })}
-          </div>
+    <div className="mx-auto mt-32">
+      <div className="w-1/2 mx-auto text-center"></div>
+      <div className="mt-3">
+        {/* DBから取得したメモデータの一覧表示 */}
+        <div className="grid w-4/5 mx-auto gap-4 grid-cols-2">
+          {categories.map((category: Category, index) => {
+            return (
+              <div className="bg-gray-100 shadow-lg mb-5 p-4">
+                <p className="text-lg font-bold mb-1">{category.name}</p>
+              </div>
+            )
+          })}
         </div>
       </div>
-    </AppLayout>
+    </div>
   )
 }
 
