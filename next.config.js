c19ec339f20f5556e05e706dd325f096a18d8b6f
uni51 <unieye51@gmail.com>
2023-07-05 18:19:46 +0900

    Tailwindのレイアウト崩れ対応

--- next.config.js
+++ next.config.js
@@ -1,26 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  swcMinify: false,
   experimental: {
     appDir: false,
-  },
-  async redirects() {
-    return [
-      // {
-      //   source: '/memos', // リダイレクト元のURL
-      //   destination: '/memos/page/1', // リダイレクト先のURL
-      //   permanent: true, // 永続的なリダイレクトかのフラグ
-      // },
-      // {
-      //   source: '/dashboard/memos', // リダイレクト元のURL
-      //   destination: '/dashboard/memos/page/1', // リダイレクト先のURL
-      //   permanent: true, // 永続的なリダイレクトかのフラグ
-      // },
-      // {
-      //   source: '/memos/categories', // リダイレクト元のURL
-      //   destination: '/memos/categories/1/page/1', // リダイレクト先のURL
-      //   permanent: true, // 永続的なリダイレクトかのフラグ
-      // },
-    ]
   },
 }
 
