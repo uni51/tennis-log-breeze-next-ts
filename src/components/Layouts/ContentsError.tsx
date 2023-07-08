debcb24423b2bb23f73d0e7e1ee4cfe6e6a22bad
uni51 <unieye51@gmail.com>
2023-07-08 10:14:30 +0900

    コンテンツエラー表示コンポーネント微修正

--- src/components/Layouts/ContentsError.tsx
+++ src/components/Layouts/ContentsError.tsx
@@ -1,21 +1,17 @@
 import AppLayout from '@/components/Layouts/AppLayout'
 
-export const ContentsError = ({ name, message, status, config, stack, headline }: any) => {
-  // const headline = `みんなの公開中のメモ一覧`
-
+/** TODO 本番運用時には、エラーの詳細を非表示にして、代わりに、一般的なメッセージを出すこと */
+export const ContentsError = ({ headline, name, message, status, config, stack }: any) => {
   return (
     <AppLayout
       header={<h2 className='font-semibold text-xl text-gray-800 leading-tight'>{headline}</h2>}
     >
       <div>
-        <div>{name}</div>
-        <p>{message}</p>
-        <div>
-          <p>{status}</p>
-          <p>{config.url}</p>
-          <p>{stack}</p>
-        </div>
-        {/* <div>エラーが発生しました</div> */}
+        <div>{name ?? 'Error'}</div>
+        <p>{message ?? 'Errorが発生しました'}</p>
+        <p>status code : {status ?? 500}</p>
+        {/* {config.url && <p>{config.url}</p>} */}
+        {/* {stack && <p>{stack}</p>} */}
       </div>
     </AppLayout>
   )
