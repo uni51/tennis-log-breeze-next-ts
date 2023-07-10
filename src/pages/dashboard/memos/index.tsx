cf6d3193d782fb5b0290872d5f87629df4d093e2
uni51 <unieye51@gmail.com>
2023-07-10 22:07:08 +0900

    ダッシュボードメモ一覧ページの微修正

--- src/pages/dashboard/memos/index.tsx
+++ src/pages/dashboard/memos/index.tsx
@@ -68,42 +68,43 @@ const DashboardMemoList: NextPage = () => {
     : ' 作成したメモ一覧'
 
   return (
-    <AppLayout
-      header={<h2 className='font-semibold text-xl text-gray-800 leading-tight'>{headline}</h2>}
-    >
+    <>
       <Head>
         <title>{headline}</title>
       </Head>
-      <div className='mx-auto mt-20'>
-        <AddMemo />
-
-        <div className='mt-3'>
-          {/* DBから取得したメモデータの一覧表示 */}
-          <div className='grid w-4/5 mx-auto gap-16 lg:grid-cols-2'>
-            {memos?.data?.map((memo: Memo, index) => {
-              return (
-                <SingleMemoBlockForList
-                  memo={memo}
-                  renderMemoDetailLink={`/dashboard/memos/${memo.id}`}
-                  renderMemoListByCategoryLink={`/dashboard/memos?category=${memo.category_id}`}
-                  renderMemoListByNickNameLink={`/dashboard/memos/`}
-                  key={index}
-                />
-              )
-            })}
+      <AppLayout
+        header={<h2 className='font-semibold text-xl text-gray-800 leading-tight'>{headline}</h2>}
+      >
+        <div className='mx-auto mt-20'>
+          <AddMemo />
+          <div className='mt-3'>
+            {/* DBから取得したメモデータの一覧表示 */}
+            <div className='grid w-4/5 mx-auto gap-16 lg:grid-cols-2'>
+              {memos?.data?.map((memo: Memo, index) => {
+                return (
+                  <SingleMemoBlockForList
+                    memo={memo}
+                    renderMemoDetailLink={`/dashboard/memos/${memo.id}`}
+                    renderMemoListByCategoryLink={`/dashboard/memos?category=${memo.category_id}`}
+                    renderMemoListByNickNameLink={`/dashboard/memos/`}
+                    key={index}
+                  />
+                )
+              })}
+            </div>
+            <MemoListPaginationAdapter
+              baseUrl='/dashboard/memos/'
+              totalItems={Number(memos?.meta?.total)}
+              currentPage={Number(memos?.meta?.current_page)}
+              renderPagerLinkFunc={
+                categoryNumber === undefined ? getMemosListPageLink : getMemosListByCategoryPageLink
+              }
+              category={categoryNumber}
+            />
           </div>
-          <MemoListPaginationAdapter
-            baseUrl='/dashboard/memos/'
-            totalItems={Number(memos?.meta?.total)}
-            currentPage={Number(memos?.meta?.current_page)}
-            renderPagerLinkFunc={
-              categoryNumber === undefined ? getMemosListPageLink : getMemosListByCategoryPageLink
-            }
-            category={categoryNumber}
-          />
         </div>
-      </div>
-    </AppLayout>
+      </AppLayout>
+    </>
   )
 }
 
