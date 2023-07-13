5cd74078b62be34b0cdea08146cc09a063c9badf
uni51 <unieye51@gmail.com>
2023-07-13 21:07:29 +0900

    フォルダリネーム用の一時コミット

--- src/components/functional/Error/CSR/ErrorFallBack/CsrErrorFallBack2.tsx
+++ src/components/functional/Error/CSR/ErrorFallBack/CsrErrorFallBack2.tsx
@@ -1,7 +1,13 @@
 import { FallbackProps } from 'react-error-boundary'
 import CsrErrorDisplay from '@/components/Layouts/Error/CSR/CsrErrorDisplay'
+import { isAxiosError } from '@/lib/utils/axiosUtils'
 
 export const CsrErrorFallback = ({ error, resetErrorBoundary }: FallbackProps) => {
+  if (isAxiosError(error) && error.response) {
+    error.response.data.status = error.response.status
+    error = error.response.data
+  }
+
   return (
     <>
       <CsrErrorDisplay {...error} resetErrorBoundary={resetErrorBoundary} />
