e2fc78344d587f3e3d8b41b784ef1876cb898923
uni51 <unieye51@gmail.com>
2023-07-30 16:24:02 +0900

    axiosのinterceptorsを使ったFirebaseのidToken送信ロジックの追加

--- src/lib/utils/apiClient.ts
+++ src/lib/utils/apiClient.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { getAuth } from '@firebase/auth'
 
 export const apiClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_FROM_CLIENT_BASE_URL,
@@ -6,4 +7,12 @@ export const apiClient = axios.create({
     'X-Requested-With': 'XMLHttpRequest',
   },
   withCredentials: true,
+})
+
+apiClient.interceptors.request.use(async (request) => {
+  //リクエスト前に毎回idTokenを取得する
+  const idToken = await getAuth().currentUser?.getIdToken()
+  //console.log(currentUser.accessToken)
+  request.headers.Authorization = idToken
+  return request
 })
