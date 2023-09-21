bb0a0b9d1ce94996026a15168e3d149d89b08396
uni51 <unieye51@gmail.com>
2023-09-21 11:44:40 +0900

    firebase認証をCookie認証に変更したのに伴い、不要になったaxiosのinterceptorsの処理を削除

--- src/lib/utils/apiClient.ts
+++ src/lib/utils/apiClient.ts
@@ -1,7 +1,4 @@
 import axios from 'axios'
-import Cookie from 'universal-cookie'
-
-const cookie = new Cookie()
 
 export const apiClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_FROM_CLIENT_BASE_URL,
@@ -10,13 +7,3 @@ export const apiClient = axios.create({
   },
   withCredentials: true,
 })
-
-// ヘッダーにAuthorizationを付与すると、Sanctumの場合、
-// ログアウト時に、personal_access_tokensにSQLを発行してしまう。
-// apiClient.interceptors.request.use((request) => {
-//   //リクエスト前に毎回idTokenを取得する
-//   // const idToken = localStorage.getItem('idToken')
-//   const idToken = cookie.get('appToken')
-//   request.headers!.Authorization = `Bearer ${idToken}`
-//   return request
-// })
