babea474bac84dc59ae72ee53bb488877bbb6def
uni51 <unieye51@gmail.com>
2023-12-02 17:37:21 +0900

    Profileページのリファクタ

--- src/pages/settings/profile.tsx
+++ src/pages/settings/profile.tsx
@@ -1,98 +1,77 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
 import AppLayout from '@/components/Layouts/AppLayout'
 import { Loading } from '@/components/Loading'
 import ProfileEdit from '@/features/settings/profile/ProfileEdit'
 import { useAuthQuery } from '@/hooks/authQuery'
-import useCheckLoggedIn from '@/hooks/checkLoggedIn'
 import { useQueryAgeRanges } from '@/hooks/profile/useQueryAgeRanges'
 import { useQueryCareers } from '@/hooks/profile/useQueryCareers'
 import { useQueryDominantHands } from '@/hooks/profile/useQueryDominantHands'
 import { useQueryGenders } from '@/hooks/profile/useQueryGenders'
 import { useQueryPlayFrequencies } from '@/hooks/profile/useQueryPlayFrequencies'
 import { useQueryTennisLevels } from '@/hooks/profile/useQueryTennisLevels'
+import { AuthGuard } from '@/features/auth/components/AuthGuard'
 
 const Profile: NextPage = () => {
-  const router = useRouter()
   const { user } = useAuthQuery({ middleware: 'auth' })
-  const checkLoggedIn = useCheckLoggedIn()
-  const [isLoading, setIsLoading] = useState(true)
-  const { status: careersStatus, data: careers, error: careersError } = useQueryCareers()
 
-  const { status: gendersStatus, data: genders, error: gendersError } = useQueryGenders()
-  // プルダウンの「選択してください」をフロント側で設定する場合は、ここでoption（デフォルト値）を追加する
+  // Custom Hooksを使用してローディングステータスを管理
+  const careersQuery = useQueryCareers()
+  const gendersQuery = useQueryGenders()
+  const ageRangesQuery = useQueryAgeRanges()
+  const dominantHandsQuery = useQueryDominantHands()
+  const playFrequenciesQuery = useQueryPlayFrequencies()
+  const tennisLevelsQuery = useQueryTennisLevels()
 
-  const { status: ageRangesStatus, data: ageRanges, error: ageRangesError } = useQueryAgeRanges()
-  const {
-    status: dominantHandsStatus,
-    data: dominantHands,
-    error: dominantHandsError,
-  } = useQueryDominantHands()
-  const {
-    status: playFrequenciesStatus,
-    data: palyFrequencies,
-    error: playFrequenciesError,
-  } = useQueryPlayFrequencies()
-  const {
-    status: tennisLevelsStatus,
-    data: tennisLevels,
-    error: tennisLevelsError,
-  } = useQueryTennisLevels()
+  // ローディングステータスを配列にまとめ、someでいずれかがpendingかどうかを判定
+  const anyPending = [
+    careersQuery,
+    gendersQuery,
+    ageRangesQuery,
+    dominantHandsQuery,
+    playFrequenciesQuery,
+    tennisLevelsQuery,
+  ].some((query) => query.status === 'pending')
 
-  useEffect(() => {
-    const res: boolean = checkLoggedIn()
-    if (!res) {
-      router.push('/login')
-      return
-    }
-    setIsLoading(false)
-  }, [])
+  if (anyPending) return <Loading />
 
-  const anyPending =
-    careersStatus === 'pending' ||
-    gendersStatus === 'pending' ||
-    ageRangesStatus === 'pending' ||
-    dominantHandsStatus === 'pending' ||
-    playFrequenciesStatus === 'pending' ||
-    tennisLevelsStatus === 'pending'
-
-  if (isLoading || anyPending) return <Loading />
-
+  // エラーハンドリングの共通関数
   const renderError = (error: Error, dataType: string) => (
     <div>
       Error fetching {dataType} data: {error.message}
     </div>
   )
 
-  if (careersError) return renderError(careersError, 'careers')
-  if (gendersError) return renderError(gendersError, 'genders')
-  if (ageRangesError) return renderError(ageRangesError, 'ageRanges')
-  if (dominantHandsError) return renderError(dominantHandsError, 'dominantHands')
-  if (playFrequenciesError) return renderError(playFrequenciesError, 'playFrequencies')
-  if (tennisLevelsError) return renderError(tennisLevelsError, 'tennisLevels')
+  // エラーがあれば該当する関数を呼び出してエラーメッセージを表示
+  if (careersQuery.error) return renderError(careersQuery.error, 'careers')
+  if (gendersQuery.error) return renderError(gendersQuery.error, 'genders')
+  if (ageRangesQuery.error) return renderError(ageRangesQuery.error, 'ageRanges')
+  if (dominantHandsQuery.error) return renderError(dominantHandsQuery.error, 'dominantHands')
+  if (playFrequenciesQuery.error) return renderError(playFrequenciesQuery.error, 'playFrequencies')
+  if (tennisLevelsQuery.error) return renderError(tennisLevelsQuery.error, 'tennisLevels')
   if (!user) return null
 
   return (
-    <AppLayout
-      header={
-        <h2 className='font-semibold text-xl text-gray-800 leading-tight'>プロフィールの編集</h2>
-      }
-    >
-      <Head>
-        <title>プロフィールの編集</title>
-      </Head>
-      <ProfileEdit
-        user={user}
-        careers={careers!}
-        genders={genders!}
-        ageRanges={ageRanges!}
-        dominantHands={dominantHands!}
-        playFrequencies={palyFrequencies!}
-        tennisLevels={tennisLevels!}
-      />
-    </AppLayout>
+    <AuthGuard>
+      <AppLayout
+        header={
+          <h2 className='font-semibold text-xl text-gray-800 leading-tight'>プロフィールの編集</h2>
+        }
+      >
+        <Head>
+          <title>プロフィールの編集</title>
+        </Head>
+        <ProfileEdit
+          user={user}
+          careers={careersQuery.data!}
+          genders={gendersQuery.data!}
+          ageRanges={ageRangesQuery.data!}
+          dominantHands={dominantHandsQuery.data!}
+          playFrequencies={playFrequenciesQuery.data!}
+          tennisLevels={tennisLevelsQuery.data!}
+        />
+      </AppLayout>
+    </AuthGuard>
   )
 }
 
