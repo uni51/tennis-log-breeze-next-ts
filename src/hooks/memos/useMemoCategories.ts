47d1d55c353fcc4f3068b424a7649c80ebeb7714
uni51 <unieye51@gmail.com>
2023-12-03 13:05:29 +0900

    useMemoDetail と useMemoCategories に try-catch 追加

--- src/hooks/memos/useMemoCategories.ts
+++ src/hooks/memos/useMemoCategories.ts
@@ -2,19 +2,29 @@ import { useQuery } from '@tanstack/react-query'
 import { apiClient } from '@/lib/utils/apiClient'
 import { Category } from '@/types/Category'
 
-const getCategories = async (): Promise<Category[]> => {
-  const response = await apiClient.get('api/memos/categories')
-  const categoriesObject = response.data.data
+const fetchMemoCategories = async (): Promise<Category[]> => {
+  try {
+    const response = await apiClient.get('api/memos/categories')
+    const categoriesObject = response.data.data
 
-  const categoriesArray = Object.values(categoriesObject) as Category[]
+    const categoriesArray = Object.values(categoriesObject) as Category[]
 
-  return categoriesArray
+    return categoriesArray
+  } catch (error) {
+    if (error instanceof Error) {
+      // エラーハンドリング: エラーが発生した場合は適切に処理する
+      throw new Error(`Failed to fetch memo categories: ${error.message}`)
+    } else {
+      // error is not an instance of Error
+      throw new Error(`Failed to fetch memo categories: ${String(error)}`)
+    }
+  }
 }
 
 export const useMemoCategories = () => {
   return useQuery<Category[], Error>({
     queryKey: ['categories'],
-    queryFn: getCategories,
+    queryFn: fetchMemoCategories,
     staleTime: Infinity,
   })
 }
--- src/hooks/memos/useMemoDetail.ts
+++ src/hooks/memos/useMemoDetail.ts
@@ -3,8 +3,18 @@ import { apiClient } from '@/lib/utils/apiClient'
 import { Memo } from '@/types/Memo'
 
 const fetchMemoDetail = async (apiUrl: string): Promise<Memo> => {
-  const { data } = await apiClient.get(apiUrl)
-  return data.data
+  try {
+    const { data } = await apiClient.get(apiUrl)
+    return data.data
+  } catch (error) {
+    if (error instanceof Error) {
+      // エラーハンドリング: エラーが発生した場合は適切に処理する
+      throw new Error(`Failed to fetch memo detail: ${error.message}`)
+    } else {
+      // error is not an instance of Error
+      throw new Error(`Failed to fetch memo detail: ${String(error)}`)
+    }
+  }
 }
 
 export const useMemoDetail = (apiUrl: string) => {
