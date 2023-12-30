7db98cee7ee3342d55d3ce17b9ebeabe45280cf6
uni51 <unieye51@gmail.com>
2023-12-30 14:40:36 +0900

    カテゴリの追加に伴う対応

--- src/lib/headline-helper.ts
+++ src/lib/headline-helper.ts
@@ -1,13 +1,15 @@
 import {
-  CATEGORY_BACKHAND,
   CATEGORY_FOREHAND,
+  CATEGORY_SINGLE_BACKHAND,
+  CATEGORY_DOUBLE_BACKHAND,
+  CATEGORY_SERVE,
+  CATEGORY_RETURN,
+  CATEGORY_VOLLEY,
+  CATEGORY_SMASH,
   CATEGORY_SINGLES,
   CATEGORY_DOUBLES,
+  CATEGORY_GEAR,
   CATEGORY_OTHER,
-  CATEGORY_RETURN,
-  CATEGORY_SERVE,
-  CATEGORY_SMASH,
-  CATEGORY_VOLLEY,
 } from '@/constants/CategoryNameConst'
 
 export const getMemosListByCategoryHeadLineTitle = (categoryNumber: number | undefined | null) => {
@@ -16,19 +18,23 @@ export const getMemosListByCategoryHeadLineTitle = (categoryNumber: number | und
     case 1:
       return separator + CATEGORY_FOREHAND
     case 2:
-      return separator + CATEGORY_BACKHAND
+      return separator + CATEGORY_SINGLE_BACKHAND
     case 3:
-      return separator + CATEGORY_SERVE
+      return separator + CATEGORY_DOUBLE_BACKHAND
     case 4:
-      return separator + CATEGORY_RETURN
+      return separator + CATEGORY_SERVE
     case 5:
-      return separator + CATEGORY_VOLLEY
+      return separator + CATEGORY_RETURN
     case 6:
-      return separator + CATEGORY_SMASH
+      return separator + CATEGORY_VOLLEY
     case 7:
-      return separator + CATEGORY_SINGLES
+      return separator + CATEGORY_SMASH
     case 8:
+      return separator + CATEGORY_SINGLES
+    case 9:
       return separator + CATEGORY_DOUBLES
+    case 10:
+      return separator + CATEGORY_GEAR
     case 99:
       return separator + CATEGORY_OTHER
     default:
