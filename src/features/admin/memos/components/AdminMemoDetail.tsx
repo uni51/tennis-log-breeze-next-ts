5aab5b778a41b2573bc6dd14b0de008e4717f70d
uni51 <unieye51@gmail.com>
2023-12-31 08:09:34 +0900

    eslint --fixによる自動修正

--- src/features/admin/memos/components/AdminMemoDetail.tsx
+++ src/features/admin/memos/components/AdminMemoDetail.tsx
@@ -1,8 +1,8 @@
 import { Dispatch, SetStateAction, useEffect } from 'react'
 import { useErrorBoundary } from 'react-error-boundary'
 import ClipLoader from 'react-spinners/ClipLoader'
-import { useMemoDetail } from '@/hooks/memos/useMemoDetail'
 import AdminSingleMemoDetail from './AdminSingleMemoDetail'
+import { useMemoDetail } from '@/hooks/memos/useMemoDetail'
 
 type Props = {
   apiUrl: string
