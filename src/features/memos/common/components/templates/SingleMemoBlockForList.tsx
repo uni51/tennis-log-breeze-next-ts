189bc6b054335dd82cdabb9546f8dffb3047c438
uni51 <unieye51@gmail.com>
2024-01-04 16:31:08 +0900

    カテゴリー表示の微調整

--- src/features/memos/common/components/templates/SingleMemoBlockForList.tsx
+++ src/features/memos/common/components/templates/SingleMemoBlockForList.tsx
@@ -21,32 +21,40 @@ const SingleMemoBlockForList: FC<SingleMemoBlockForListProps> = ({
         <Link href={renderMemoDetailLink}>{memo.title}</Link>
       </p>
       <div className='border-b-2 border-gray-300 mb-4'></div>
-      <p className='mb-5 whitespace-pre-wrap'>{memo.body}</p>
-      <p className='text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 last:mr-0 mr-1'>
+      <p className='mb-3 whitespace-pre-wrap'>{memo.body}</p>
+      <p className='text-xs font-semibold inline-block py-1 px-2 uppercase rounded-lg text-pink-600 bg-pink-200 last:mr-0 mr-1'>
         <Link href={renderMemoListByCategoryLink}>{memo.category_name}</Link>
       </p>
-      <p>
+      <p className='mt-1'>
         {memo.tag_list.tags.map((tag, index) => (
-          <Link href={`/tags/${memo.tag_list.normalized[index]}`} key={index} className='pr-4'>
-            <span className='text-xs font-semibold py-1 px-2 uppercase rounded-full text-blue-600 bg-blue-200 last:mr-0 mr-1'>
+          <Link href={`/tags/${memo.tag_list.normalized[index]}`} key={index} className='pr-1'>
+            <span className='text-xs font-semibold py-1 px-2 uppercase rounded-lg text-green-600 bg-green-200 last:mr-0 mr-1'>
               {tag}
             </span>
           </Link>
         ))}
       </p>
-      <p>
-        <span className='text-xs font-semibold py-1 px-2 uppercase rounded-full text-green-600 bg-green-200 last:mr-0 mr-1'>
+      <p className='mt-1'>
+        <Link href={renderMemoListByNickNameLink}>
+          <span className='text-xs font-semibold py-1 px-2 uppercase rounded-lg text-blue-600 bg-blue-200 last:mr-0 mr-1'>
+            {memo.user_nickname}
+          </span>
+        </Link>
+      </p>
+      <p className='mt-1'>
+        <span className='text-xs font-semibold py-1 px-2 uppercase rounded-lg text-white bg-black last:mr-0 mr-1'>
           {memo.status === 0 && '下書き'}
           {memo.status === 1 && '公開中'}
           {memo.status === 2 && 'シェア'}
           {memo.status === 3 && '非公開'}
         </span>
       </p>
-      <p className='text-sm leading-6 text-gray-500 mt-2'>
-        作成者：
-        <Link href={renderMemoListByNickNameLink}>{memo.user_nickname}</Link>
+      <p className='text-sm leading-6 text-gray-500 mt-2 inline-block pr-4'>
+        作成日時：{memo.created_at}
       </p>
-      <p className='text-sm leading-6 text-gray-500 mt-2'>更新日時：{memo.updated_at}</p>
+      <p className='text-sm leading-6 text-gray-500 mt-2 inline-block'>
+        更新日時：{memo.updated_at}
+      </p>
     </div>
   )
 }
