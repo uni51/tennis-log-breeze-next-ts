0b466f6c97ed7b2196cc9aa75528122ce425dd5e
uni51 <unieye51@gmail.com>
2024-06-25 10:53:16 +0900

    ある大項目配下の項目を選択してページ遷移した場合に、そのエリアを開いたままの状態にする対応

--- src/components/Layouts/StaticSidebar.tsx
+++ src/components/Layouts/StaticSidebar.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { Disclosure } from '@headlessui/react'
 import { navigation } from '@/lib/navigation/navigationData'
 import { classNames } from '@/lib/utils/utils'
 
 const StaticSidebar: React.FC = () => {
+  const router = useRouter()
+
+  // URLがMemos配下のアイテムと一致するかチェック
+  const checkIfMemosActive = () => {
+    return navigation
+      .find((item) => item.name === 'Memos')
+      ?.children?.some((subItem) => router.pathname === subItem.href)
+  }
+
   return (
     <div className='hidden md:fixed md:inset-y-0 md:flex md:w-64 md:flex-col'>
       <div className='flex flex-grow flex-col overflow-y-auto border-r border-gray-200 bg-white pt-5 pb-4'>
@@ -19,73 +29,54 @@ const StaticSidebar: React.FC = () => {
           />
         </div>
         <nav className='mt-5 flex flex-grow flex-col bg-white px-2' aria-label='Sidebar'>
-          {navigation.map((item) =>
-            !item.children ? (
-              <Link
-                key={item.name}
-                href={item.href}
-                className={classNames(
-                  item.current
-                    ? 'bg-gray-100 text-gray-900'
-                    : 'bg-white text-gray-600 hover:bg-gray-50 hover:text-gray-900',
-                  'group w-full flex items-center pl-2 py-2 text-sm font-medium rounded-md',
-                )}
-              >
-                <item.icon
-                  className={classNames(
-                    item.current ? 'text-gray-500' : 'text-gray-400 group-hover:text-gray-500',
-                    'mr-3 flex-shrink-0 h-6 w-6',
-                  )}
-                  aria-hidden='true'
-                />
-                {item.name}
-              </Link>
-            ) : (
-              <Disclosure as='div' key={item.name} className='space-y-1'>
-                {({ open }) => (
-                  <>
-                    <Disclosure.Button
+          {navigation.map((item) => (
+            <Disclosure
+              defaultOpen={item.name === 'Memos' && checkIfMemosActive()}
+              as='div'
+              key={item.name}
+              className='space-y-1'
+            >
+              {({ open }) => (
+                <>
+                  <Disclosure.Button
+                    className={classNames(
+                      'group w-full flex items-center pl-2 pr-1 py-2 text-left text-sm font-medium rounded-md focus:outline-none',
+                      item.current || (item.name === 'Memos' && checkIfMemosActive())
+                        ? 'bg-gray-100 text-gray-900'
+                        : 'bg-white text-gray-600 hover:bg-gray-50 hover:text-gray-900',
+                    )}
+                  >
+                    <item.icon
+                      className='mr-3 h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500'
+                      aria-hidden='true'
+                    />
+                    <span className='flex-1'>{item.name}</span>
+                    <svg
                       className={classNames(
-                        item.current
-                          ? 'bg-gray-100 text-gray-900'
-                          : 'bg-white text-gray-600 hover:bg-gray-50 hover:text-gray-900',
-                        'group w-full flex items-center pl-2 pr-1 py-2 text-left text-sm font-medium rounded-md focus:outline-none',
+                        open ? 'text-gray-400 rotate-90' : 'text-gray-300',
+                        'ml-3 h-5 w-5 flex-shrink-0 transform transition-colors duration-150 ease-in-out group-hover:text-gray-400',
                       )}
+                      viewBox='0 0 20 20'
+                      aria-hidden='true'
                     >
-                      <item.icon
-                        className='mr-3 h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500'
-                        aria-hidden='true'
-                      />
-
-                      <span className='flex-1'>{item.name}</span>
-                      <svg
-                        className={classNames(
-                          open ? 'text-gray-400 rotate-90' : 'text-gray-300',
-                          'ml-3 h-5 w-5 flex-shrink-0 transform transition-colors duration-150 ease-in-out group-hover:text-gray-400',
-                        )}
-                        viewBox='0 0 20 20'
-                        aria-hidden='true'
+                      <path d='M6 6L14 10L6 14V6Z' fill='currentColor' />
+                    </svg>
+                  </Disclosure.Button>
+                  <Disclosure.Panel className='space-y-1'>
+                    {item.children?.map((subItem) => (
+                      <Link
+                        key={subItem.name}
+                        href={subItem.href}
+                        className='group flex w-full items-center rounded-md py-2 pl-11 pr-2 text-sm font-medium text-gray-600 hover:bg-gray-50 hover:text-gray-900'
                       >
-                        <path d='M6 6L14 10L6 14V6Z' fill='currentColor' />
-                      </svg>
-                    </Disclosure.Button>
-                    <Disclosure.Panel className='space-y-1'>
-                      {item.children.map((subItem) => (
-                        <Disclosure.Button
-                          key={subItem.name}
-                          as='a'
-                          href={subItem.href}
-                          className='group flex w-full items-center rounded-md py-2 pl-11 pr-2 text-sm font-medium text-gray-600 hover:bg-gray-50 hover:text-gray-900'
-                        >
-                          {subItem.name}
-                        </Disclosure.Button>
-                      ))}
-                    </Disclosure.Panel>
-                  </>
-                )}
-              </Disclosure>
-            ),
-          )}
+                        {subItem.name}
+                      </Link>
+                    ))}
+                  </Disclosure.Panel>
+                </>
+              )}
+            </Disclosure>
+          ))}
         </nav>
       </div>
     </div>
