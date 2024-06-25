85796fbb8951e6a6515cb04ead0c20754fa3f986
uni51 <unieye51@gmail.com>
2024-06-25 19:59:33 +0900

    管理者用サイドメニューの表示修正

--- src/components/Layouts/Admin/AdminStaticSidebar.tsx
+++ src/components/Layouts/Admin/AdminStaticSidebar.tsx
@@ -3,17 +3,18 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { Disclosure } from '@headlessui/react'
-import { classNames } from '@/lib/utils/utils'
 import { adminNavigation } from '@/lib/navigation/adminNavigationData'
+import { classNames } from '@/lib/utils/utils'
 
 const AdminStaticSidebar: React.FC = () => {
   const router = useRouter()
 
-  // URLがMemos配下のアイテムと一致するかチェック
-  const checkIfMemosActive = () => {
-    return adminNavigation
-      .find((item) => item.name === 'Memos')
-      ?.children?.some((subItem) => router.pathname === subItem.href)
+  // ナビゲーション項目が現在のパスに一致するかどうかをチェック
+  const isActive = (href: string | undefined) => router.asPath === href
+
+  // 任意のナビゲーション項目の子がアクティブかどうかをチェック
+  const isChildrenActive = (children: any[]) => {
+    return children?.some((subItem) => isActive(subItem.href))
   }
 
   return (
@@ -29,54 +30,78 @@ const AdminStaticSidebar: React.FC = () => {
           />
         </div>
         <nav className='mt-5 flex flex-grow flex-col bg-white px-2' aria-label='Sidebar'>
-          {adminNavigation.map((item) => (
-            <Disclosure
-              defaultOpen={item.name === 'Memos' && checkIfMemosActive()}
-              as='div'
-              key={item.name}
-              className='space-y-1'
-            >
-              {({ open }) => (
-                <>
-                  <Disclosure.Button
-                    className={classNames(
-                      'group w-full flex items-center pl-2 pr-1 py-2 text-left text-sm font-medium rounded-md focus:outline-none',
-                      item.current || (item.name === 'Memos' && checkIfMemosActive())
-                        ? 'bg-gray-100 text-gray-900'
-                        : 'bg-white text-gray-600 hover:bg-gray-50 hover:text-gray-900',
-                    )}
-                  >
-                    <item.icon
-                      className='mr-3 h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500'
-                      aria-hidden='true'
-                    />
-                    <span className='flex-1'>{item.name}</span>
-                    <svg
+          {adminNavigation.map((item) =>
+            item.children ? (
+              <Disclosure
+                defaultOpen={isChildrenActive(item.children)}
+                as='div'
+                key={item.name}
+                className='space-y-1'
+              >
+                {({ open }) => (
+                  <>
+                    <Disclosure.Button
                       className={classNames(
-                        open ? 'text-gray-400 rotate-90' : 'text-gray-300',
-                        'ml-3 h-5 w-5 flex-shrink-0 transform transition-colors duration-150 ease-in-out group-hover:text-gray-400',
+                        'group w-full flex items-center pl-2 pr-1 py-2 text-left text-sm font-medium rounded-md focus:outline-none',
+                        isActive(item.href) || isChildrenActive(item.children)
+                          ? 'bg-gray-100 text-gray-900'
+                          : 'bg-white text-gray-600 hover:bg-gray-50 hover:text-gray-900',
                       )}
-                      viewBox='0 0 20 20'
-                      aria-hidden='true'
                     >
-                      <path d='M6 6L14 10L6 14V6Z' fill='currentColor' />
-                    </svg>
-                  </Disclosure.Button>
-                  <Disclosure.Panel className='space-y-1'>
-                    {item.children?.map((subItem) => (
-                      <Link
-                        key={subItem.name}
-                        href={subItem.href}
-                        className='group flex w-full items-center rounded-md py-2 pl-11 pr-2 text-sm font-medium text-gray-600 hover:bg-gray-50 hover:text-gray-900'
+                      <item.icon
+                        className='mr-3 h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500'
+                        aria-hidden='true'
+                      />
+                      <span className='flex-1'>{item.name}</span>
+                      <svg
+                        className={classNames(
+                          open ? 'text-gray-400 rotate-90' : 'text-gray-300',
+                          'ml-3 h-5 w-5 flex-shrink-0 transform transition-colors duration-150 ease-in-out group-hover:text-gray-400',
+                        )}
+                        viewBox='0 0 20 20'
+                        aria-hidden='true'
                       >
-                        {subItem.name}
-                      </Link>
-                    ))}
-                  </Disclosure.Panel>
-                </>
-              )}
-            </Disclosure>
-          ))}
+                        <path d='M6 6L14 10L6 14V6Z' fill='currentColor' />
+                      </svg>
+                    </Disclosure.Button>
+                    <Disclosure.Panel className='space-y-1'>
+                      {item.children.map((subItem) => (
+                        <Link
+                          key={subItem.name}
+                          href={subItem.href}
+                          className={classNames(
+                            'group flex w-full items-center rounded-md py-2 pl-11 pr-2 text-sm font-medium',
+                            isActive(subItem.href)
+                              ? 'text-gray-900 bg-gray-50'
+                              : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900',
+                          )}
+                        >
+                          {subItem.name}
+                        </Link>
+                      ))}
+                    </Disclosure.Panel>
+                  </>
+                )}
+              </Disclosure>
+            ) : (
+              <Link
+                key={item.name}
+                href={item.href}
+                className={classNames(
+                  'group w-full flex items-center pl-2 pr-1 py-2 text-left text-sm font-medium rounded-md focus:outline-none',
+                  isActive(item.href)
+                    ? 'bg-gray-100 text-gray-900'
+                    : 'bg-white text-gray-600 hover:bg-gray-50 hover:text-gray-900',
+                )}
+              >
+                <item.icon
+                  className='mr-3 h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500'
+                  aria-hidden='true'
+                />
+                <span className='flex-1'>{item.name}</span>
+              </Link>
+            ),
+          )}
         </nav>
       </div>
     </div>
--- src/lib/navigation/adminNavigationData.ts
+++ src/lib/navigation/adminNavigationData.ts
@@ -6,53 +6,24 @@ import {
   CalendarIcon,
   InboxIcon,
   ChartBarIcon,
+  DocumentTextIcon,
 } from '@heroicons/react/24/outline'
 
 export const adminNavigation = [
   { name: 'Dashboard', icon: HomeIcon, current: true, href: '/admin/dashboard' },
   {
     name: 'Memos',
-    icon: UsersIcon,
+    icon: DocumentTextIcon,
     current: false,
     children: [
       { name: 'メモ一覧', href: '/admin/memos' },
       { name: 'レビュー待ちメモ一覧', href: '/admin/memos/waiting/review' },
       { name: '修正依頼中メモ一覧', href: '/admin/memos/waiting/fix' },
-      { name: 'フォアハンド', href: '#' },
-      { name: 'バックハンド', href: '#' },
-      { name: 'サーブ', href: '#' },
-      { name: 'リターン', href: '#' },
-      { name: 'ボレー', href: '#' },
-      { name: 'スマッシュ', href: '#' },
-      { name: 'ゲーム', href: '#' },
-      { name: 'その他', href: '#' },
     ],
   },
-  // {
-  //   name: 'Projects',
-  //   icon: FolderIcon,
-  //   current: false,
-  //   children: [
-  //     { name: 'Overview', href: '#' },
-  //     { name: 'Members', href: '#' },
-  //     { name: 'Calendar', href: '#' },
-  //     { name: 'Settings', href: '#' },
-  //   ],
-  // },
-  // {
-  //   name: 'Calendar',
-  //   icon: CalendarIcon,
-  //   current: false,
-  //   children: [
-  //     { name: 'Overview', href: '#' },
-  //     { name: 'Members', href: '#' },
-  //     { name: 'Calendar', href: '#' },
-  //     { name: 'Settings', href: '#' },
-  //   ],
-  // },
   {
     name: 'Users',
-    icon: InboxIcon,
+    icon: UsersIcon,
     current: false,
     href: '/admin/users',
     children: [
