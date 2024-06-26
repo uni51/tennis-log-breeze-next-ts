667135c2be462ccea63bcf0fbad1c7c6a393e6c6
uni51 <unieye51@gmail.com>
2024-06-26 15:42:33 +0900

    プルダウンで「選択してください」が選択されている場合は、エラーを出すように修正

--- src/lib/schema/profileSchema.ts
+++ src/lib/schema/profileSchema.ts
@@ -7,33 +7,37 @@ export const ProfileSchema = z.object({
     .string()
     .min(1, { message: 'Query parameter is required' })
     .transform((val) => parseInt(val))
-    .refine((val) => !isNaN(val), { message: '不正な値です' }),
+    .refine((val) => !isNaN(val) && val !== 0, { message: 'テニス歴を選択してください' }),
   gender_id: z
     .string()
     .min(1, { message: 'Query parameter is required' })
     .transform((val) => parseInt(val))
-    .refine((val) => !isNaN(val), { message: '不正な値です' }),
-  ageRange_id: z
-    .string()
-    .min(1, { message: 'Query parameter is required' })
-    .transform((val) => parseInt(val))
-    .refine((val) => !isNaN(val), { message: '不正な値です' }),
+    .refine((val) => !isNaN(val) && val !== 0, { message: '性別を選択してください' }),
   dominantHand_id: z
     .string()
     .min(1, { message: 'Query parameter is required' })
     .transform((val) => parseInt(val))
-    .refine((val) => !isNaN(val), { message: '不正な値です' }),
+    .refine((val) => !isNaN(val) && val !== 0, { message: '利き手を選択してください' }),
   playFrequency_id: z
     .string()
     .min(1, { message: 'Query parameter is required' })
     .transform((val) => parseInt(val))
-    .refine((val) => !isNaN(val), { message: '不正な値です' }),
+    .refine((val) => !isNaN(val) && val !== 0, { message: 'プレー頻度を選択してください' }),
   tennisLevel_id: z
     .string()
     .min(1, { message: 'Query parameter is required' })
     .transform((val) => parseInt(val))
-    .refine((val) => !isNaN(val), { message: '不正な値です' }),
-  year: z.string().min(4, '年を選択してください'),
-  month: z.string().min(1, '月を選択してください'),
-  day: z.string().min(1, '日を選択してください'),
+    .refine((val) => !isNaN(val) && val !== 0, { message: 'テニスのレベルを選択してください' }),
+  year: z
+    .string()
+    .min(4, '年を選択してください')
+    .refine((val) => val !== '0000', { message: '年を選択してください' }),
+  month: z
+    .string()
+    .min(1, '月を選択してください')
+    .refine((val) => val !== '0', { message: '月を選択してください' }),
+  day: z
+    .string()
+    .min(1, '日を選択してください')
+    .refine((val) => val !== '0', { message: '日を選択してください' }),
 })
